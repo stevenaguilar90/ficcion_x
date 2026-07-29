00dac02b556de1ad563d652471d2537e5aa9e6e7
stevenaguilar90 <5062883@clases.edu.sv>
2026-07-29 07:33:03 -0600

    Agregué imports y configuración de colorama

---
+++ main.py
@@ -0,0 +1,12 @@
+from sistema.biblioteca import Biblioteca
+from archivos.json_manager import JSONManager
+from modelos.libro import Libro
+import os 
+try:
+    from colorama import init, Fore, Style
+    init(autoreset=True)
+except ImportError:
+    class Fore:
+        RED = GREEN = YELLOW = CYAN = ""
+    class Style:
+        RESET_ALL = ""
